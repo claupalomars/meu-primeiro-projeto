1f2ca225196180df54b7a4f3c08ab17e5fedf6b2
Cláudia Palomares <claudiaqa29@gmail.com>
2025-11-04 00:55:33 -0300

    Fix character set and improve password generation

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -2,8 +2,9 @@ import random
 import string
 
 def gerar_senha(tamanho=10):
-  caracteres = string.ascii_letters + string.digit + string.punctuation
-  senha = ''.join(random.choices(caracteres) for i in range(tamanho))
-  return senha
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choices(caracteres, k=tamanho))
+    return senha
 
 print("Sua senha gerada é:", gerar_senha())
+
